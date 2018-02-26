eec1e47378c6370874cdb18953ea715da09c93ba
losjun <ledo@ledodeMac-mini-3.local>
2018-02-26 19:35:55 +0800

    Add ResourceManager

---
+++ OpenGL_Xcode/OpenGL/include/game.h
@@ -0,0 +1,27 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+enum GameState{
+    GAME_ACTIVE,
+    GAME_MENU,
+    GAME_WIN
+};
+
+class Game
+{
+public:
+    GameState State;
+    bool    Keys[1024];
+    int     Width, Height;
+    Game(int width, int height);
+    ~Game();
+    void Init();
+    void ProcessInput(float delta);
+    void Update(float delta);
+    void Render();
+};
+
+#endif
---
+++ OpenGL_Xcode/OpenGL/src/game.cpp
@@ -0,0 +1,30 @@
+#include "game.h"
+//#include "resource_manager.h"
+
+Game::Game(int width, int height)
+    : State(GAME_ACTIVE), Keys(), Width(width), Height(height)
+{
+    
+}
+
+Game::~Game()
+{
+    
+}
+
+void Game::Init()
+{
+    
+}
+
+void Game::Update(float delta)
+{
+    
+}
+
+void Game::ProcessInput(float delta)
+{
+    
+}
+
+
